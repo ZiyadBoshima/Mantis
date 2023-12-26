963bd1da93bd4ac8ace7e336156a1f595d346000
Ziyad <137479354+ZiyadBoshima@users.noreply.github.com>
2023-12-26 18:44:40 +0100

    Show issue details

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -25,7 +25,9 @@ const IssuesPage = async () => {
           {issues.map(issue => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>
+                  {issue.title}
+                </Link>
                 <div className='block md:hidden'><IssueStatusBadge status={issue.status} /></div>
               </Table.Cell>
               <Table.Cell className='hidden md:table-cell'><IssueStatusBadge status={issue.status} /></Table.Cell>
