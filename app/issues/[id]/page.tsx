3c3569acd139e6944ed52b6505ead6e2ad43aab0
Ziyad <137479354+ZiyadBoshima@users.noreply.github.com>
2023-12-26 19:11:46 +0100

    Add additional loading skeletons

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -2,7 +2,6 @@ import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import prisma from '@/prisma/client'
 import { Card, Flex, Heading, Text } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
-import React from 'react'
 import ReactMarkdown from 'react-markdown'
 
 interface Props {
