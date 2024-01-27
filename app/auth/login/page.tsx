03102174d579d51c422c992e61919617e7eb9e3b
Ziyad <137479354+ZiyadBoshima@users.noreply.github.com>
2024-01-27 11:49:19 +0100

    Add error handling and callout in login

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Heading, TextField, Button } from "@radix-ui/themes"
+import { Heading, TextField, Button, Callout, Flex } from "@radix-ui/themes"
 import FormWrapper from "../FormWrapper"
 import { userLoginSchema } from "@/app/validationSchemas"
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -8,12 +8,15 @@ import { useForm } from "react-hook-form"
 import { useState } from "react"
 import { z } from "zod"
 import { signIn } from "next-auth/react"
-import { useRouter } from "next/navigation"
+import { useSearchParams } from "next/navigation"
+import SuccessMessage from "@/app/components/SuccessMessage"
+import { ErrorMessage, Spinner } from "@/app/components"
 
 type UserFormData = z.infer<typeof userLoginSchema>
 
 const Login = () => {
-  const router = useRouter();
+  const params = useSearchParams()
+
   const { register, handleSubmit, formState: { errors } } = useForm<UserFormData>({
     resolver: zodResolver(userLoginSchema)
   })
@@ -23,6 +26,9 @@ const Login = () => {
 
   const onSubmit = handleSubmit(async (data) => {
     try {
+      setError('')
+      setSubmitting(true)
+
       await signIn('credentials', {
         email: data.email,
         password: data.password,
@@ -30,23 +36,45 @@ const Login = () => {
         callbackUrl: '/'
       })
     } catch(error) {
+      setSubmitting(false)
+      setError('An unexpected error occured. Please try again.')
     }
   })
 
   return (
-    <FormWrapper type='LOGIN'> 
-      <form className="flex flex-col gap-4" onSubmit={onSubmit}>
-        <section>
-          <Heading size="3" mb="2">Email</Heading>
-          <TextField.Input type="email" placeholder="Email" size="3" {...register('email')} />
-        </section>
-        <section> 
-          <Heading size="3" mb="2">Password</Heading>
-          <TextField.Input type="password" placeholder="Password" size="3" {...register('password')} />
-        </section>
-        <Button type="submit" size="3" className="w-full" mt="4">Login</Button>
-      </form>
-    </FormWrapper>
+    <>
+      <FormWrapper type='LOGIN'> 
+        <form className="flex flex-col gap-4" onSubmit={onSubmit}>
+          <section>
+            <Flex align='baseline' justify='between'>
+              <Heading size="3" mb="2">Email</Heading>
+              {errors.email?.message && (<ErrorMessage>{errors.email?.message}</ErrorMessage>)}
+            </Flex>
+            <TextField.Input type="email" placeholder="Email" size="3" {...register('email')} />
+          </section>
+          <section> 
+            <Flex align='baseline' justify='between'>
+              <Heading size="3" mb="2">Password</Heading>
+              {errors.password?.message && (<ErrorMessage>{errors.password?.message}</ErrorMessage>)}
+            </Flex>
+            <TextField.Input type="password" placeholder="Password" size="3" {...register('password')} />
+          </section>
+          <Button disabled={isSubmitting} type="submit" size="3" className="w-full" mt="4">
+            {isSubmitting ? <Spinner /> : 'Login'}
+          </Button>
+        </form>
+      </FormWrapper>
+      {(params.get('signup') && !error) && (
+        <Callout.Root color="green" className='max-w-md mx-auto my-2'>
+          <Callout.Text>Sign up successful! Please login to continue.</Callout.Text>
+        </Callout.Root>
+      )}
+      {error && (
+        <Callout.Root color="red" className='max-w-md mx-auto my-2'>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+    </>
   )
 }
 
